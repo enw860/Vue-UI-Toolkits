3afde9c719372520792a69a6fc76b1664693edc5
lionel-wu@github.ibm.com <lionel.wu@ibm.com>
2021-03-07 16:49:33 -0500

    modulize label wrapper

--- src/components/wrapper/index.js
+++ src/components/wrapper/index.js
@@ -1,9 +1,11 @@
 import { pluginFactory } from "../../utils/plugins";
 
 import Dropdown from "./dropDown";
+import LabelWrapper from "./labelWrapper";
 
 export default pluginFactory({
     plugins: [
         Dropdown,
+        LabelWrapper,
     ]
 })
---
+++ src/components/wrapper/labelWrapper/index.js
@@ -0,0 +1,8 @@
+import LabelWrapper from "./LabelWrapper.vue";
+import { pluginFactory } from "../../../utils/plugins";
+
+export default pluginFactory({
+    components: [
+        LabelWrapper
+    ]
+})
