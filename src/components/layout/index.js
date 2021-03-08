07635c21f265f6ad32f5fe657c9cfc01b6fddb75
lionel-wu@github.ibm.com <lionel.wu@ibm.com>
2021-03-07 21:36:45 -0500

    modulize slideout

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -2,11 +2,12 @@ import { pluginFactory } from "../../utils/plugins";
 
 import MaskLayer from "./maskLayer";
 import Separator from "./separator";
-
+import Slideout from "./slideout";
 
 export default pluginFactory({
     plugins: [
         MaskLayer,
-        Separator
+        Separator,
+        Slideout
     ]
 })
---
+++ src/components/layout/slideout/index.js
@@ -0,0 +1,8 @@
+import Slideout from "./Slideout.vue";
+import { pluginFactory } from "../../../utils/plugins";
+
+export default pluginFactory({
+    components: [
+        Slideout
+    ]
+})
