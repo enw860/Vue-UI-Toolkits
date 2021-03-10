7aedf153b950a0bf14e7987bf275ef559e01f7ea
lionel-wu@github.ibm.com <lionel.wu@ibm.com>
2021-03-10 00:41:22 -0500

    imporve documentation - separator

--- src/components/layout/separator/Separator.vue
+++ src/components/layout/separator/Separator.vue
@@ -10,34 +10,41 @@
 import "./Separator.less";
 import { normalizeInput } from "../../../utils/utilities";
 
+const DIRECTION = {
+	horizontal: "divide-horizontal",
+	vertical: "divide-vertical",
+};
+
 export default {
 	controlName: "l-separator",
+
 	name: "Separator",
+
 	data: function () {
-		return {
-			DIRECTION: {
-				horizontal: "divide-horizontal",
-				vertical: "divide-vertical",
-			},
-		};
+		return {};
 	},
+
 	props: {
 		direction: {
 			type: String,
 			default: "horizontal",
+			description: "Direction of the separator",
+			options: Object.keys(DIRECTION),
 		},
 		color: {
 			type: String,
-			default: "",
+			default: "#000000",
+			description: "Color of the separator.",
 		},
 		thickness: {
 			type: Number,
-			default: 0,
+			default: 1,
+			description: "Thickness of the separator.",
 		},
 	},
 	computed: {
 		directionClass: function () {
-			return normalizeInput(this.DIRECTION, this.direction);
+			return normalizeInput(DIRECTION, this.direction);
 		},
 		style: function () {
 			return {
