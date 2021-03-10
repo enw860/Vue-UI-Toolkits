d06eaa5886239e36ee612be733795acefb2efa71
lionel-wu@github.ibm.com <lionel.wu@ibm.com>
2021-03-10 00:24:28 -0500

    imporve documentation - text

--- src/components/infomational/displayText/DisplayText.vue
+++ src/components/infomational/displayText/DisplayText.vue
@@ -13,50 +13,57 @@
 import "./DisplayText.less";
 import { normalizeInput } from "../../../utils/utilities";
 
+const SIZE = {
+	auto: "",
+	xsmall: "size-xsmall",
+	small: "size-small",
+	default: "size-normal",
+	normal: "size-normal",
+	large: "size-large",
+	xlarge: "size-xlarge",
+	xxlarge: "size-xxlarge",
+	xxxlarge: "size-xxxlarge",
+};
+
 export default {
 	controlName: "l-text",
 
 	name: "DisplayText",
 
 	data: function () {
-		return {
-			SIZE: {
-				auto: "",
-				xsmall: "size-xsmall",
-				small: "size-small",
-				default: "size-normal",
-				normal: "size-normal",
-				large: "size-large",
-				xlarge: "size-xlarge",
-				xxlarge: "size-xxlarge",
-				xxxlarge: "size-xxxlarge",
-			},
-		};
+		return {};
 	},
+
 	props: {
 		value: {
 			type: String,
 			default: "auto",
+			description: "Content of display.",
 		},
 		size: {
 			type: String,
 			default: "default",
+			description: "Size of the text.",
+			options: Object.keys(SIZE),
 		},
 		fontWeight: {
 			type: Number,
 			default: 400,
+			description: "Font weight of the text.",
 		},
 		color: {
 			type: String,
-			default: "",
+			default: "#000000",
+			description: "Font color of the text.",
 		},
 	},
+
 	computed: {
 		describeBy: function () {
 			return this.$parent.labelID || "";
 		},
 		sizeClass: function () {
-			return normalizeInput(this.SIZE, this.size);
+			return normalizeInput(SIZE, this.size);
 		},
 		isDisabled: function () {
 			return this.$parent.isDisabled || this.disabled;
